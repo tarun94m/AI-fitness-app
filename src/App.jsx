67966008bc6d5e573c901a2b41e77a4a9ac72278
Tarun M <sachitmanjunath98@gmail.com>
2025-02-10 14:15:31 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from "./pages/Landing";
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
